42b1e996d7f72624954e88ddcf63664ac1a4346d
LaiZiYang <127309242+LaiZiYang@users.noreply.github.com>
2023-08-29 23:28:39 +1000

    add different tetromino

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { State, Action, TetrominoBLocks } from "./types";
 import { createSvgElement } from "./views";
-import { Constants, Block, initialTetromino } from "./main";
+import { Constants, Block, oTetromino } from "./main";
 
 const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
     HTMLElement;
@@ -15,11 +15,6 @@ const svg = document.querySelector("#svgCanvas") as SVGGraphicsElement &
 class tick implements Action {
     constructor(public readonly elapsed:number){}
     apply(s: State): State {
-        const not = <T>(f:(x:T)=>boolean)=>(x:T)=>!f(x)
-        // search for a body by id in an array
-        const elem = (a:ReadonlyArray<TetrominoBLocks>) => (e:TetrominoBLocks) => a.findIndex(b=>b.id === e.id) >= 0
-        // array a except anything in b
-        const except = (a:ReadonlyArray<TetrominoBLocks>) => (b:ReadonlyArray<TetrominoBLocks>) => a.filter(not(elem(b)))
         const mergeMap = <T, U>(
             a: ReadonlyArray<T>,
             f: (a: T) => ReadonlyArray<U>
@@ -31,7 +26,7 @@ class tick implements Action {
         const collidedBlockWithBlock = () => allBottomAndPlacedBlocks().filter(t => t[0].y+1 === t[1].y && t[0].x == t[1].x).length > 0
         const createNewTetromino = (time: number) => {
             return [
-                {...initialTetromino[0], id: time}, {...initialTetromino[1], id: time+1}, {...initialTetromino[2], id: time+2}, {...initialTetromino[3], id: time+3}
+                {...oTetromino[0], id: time}, {...oTetromino[1], id: time+1}, {...oTetromino[2], id: time+2}, {...oTetromino[3], id: time+3}
             ]
         }
         const placedBlock = () => collidedBlockWithBlock() || collidedBottom() ? s.placedTetromino.concat(s.tetromino) : s.placedTetromino
@@ -40,17 +35,9 @@ class tick implements Action {
         const rowsToDelete = () => affectedBlocks().filter(a => a.length === 10).reduce((a, b) => a.concat(b), [])
         const rowsToRemain = () => affectedBlocks().filter(a => a.length < 10).reduce((a, b) => a.concat(b), [])
         const getBlocksInRow = (r: number) => placedBlock().filter(b => b.y === r)
-        const getBlocksInRowAbove = (c: TetrominoBLocks[],r: number) => c.filter(b => b.y < r)
+        const getBlocksInRowAbove = (c: TetrominoBLocks[],r: number, f: (n: TetrominoBLocks) => TetrominoBLocks) => c.map(b=> b.y < r ? f(b) : b)
         const getRowsNumberToDelete = () => rowsToDelete().reduce((a: ReadonlyArray<number>, b: TetrominoBLocks)=> a.includes(b.y) ? a : a.concat([b.y]), [])
-        const shiftRowsDown = () => rowsToDelete().length > 0 ? getRowsNumberToDelete().reduce((a,c) => getBlocksInRowAbove(a, c).map(b=> ({...b, y: b.y + 1})), [...rowsToRemain()]) : rowsToRemain()
-        // getRowsNumberToDelete().map(r=> getBlocksInRowAbove(r).map(b => ({...b, y: b.y+1}))).reduce((a,b)=> a.concat(b), [])
-        // const affectedBlocks = () => affectedRow().map(r=> placedBlock().filter(b=> b.y === r))
-        // const blocksToDelete = () => affectedBlocks().filter(s => s.length === 10)
-        // const setOfBlocksToDelete = () => blocksToDelete().reduce((a,c) => a.concat(c), [])
-        // const finalPlacedBlock = () => placedBlock().filter(b=> !(setOfBlocksToDelete().includes(b)))
-        // const updateBoard = (board: ReadonlyArray<ReadonlyArray<number>>) => {
-        //     const newBoard = [...board].reduce((a,c)=> , 0)
-        // }
+        const shiftRowsDown = () => rowsToDelete().length > 0 ? getRowsNumberToDelete().reduce((a,c) => getBlocksInRowAbove(a, c, (b) => ({...b, y: b.y + 1})), rowsToRemain()) : rowsToRemain()
         return {
             ...s,
             tetromino: !collidedBottom () ? (collidedBlockWithBlock() ? createNewTetromino(s.time) : s.tetromino.map(b=> {
@@ -65,17 +52,6 @@ class tick implements Action {
         }
     }
 }
-// = (s: State) => {
-//     return {
-//         ...s,
-//         tetromino: s.tetromino.map(b=> {
-//             return {
-//                 ...b,
-//                 y: b.y+1
-//             }
-//         })
-//     }
-// };
 
 
 
@@ -112,13 +88,13 @@ class MoveRight implements Action {
         const getRightmostBlock = (tetromino: ReadonlyArray<TetrominoBLocks>) => tetromino.reduce((m, b) => b.x > m.x ? {...b} : {...m})
         const allActiveAndPlacedBlocks = () => mergeMap(s.tetromino, b=> s.placedTetromino.map((p=> [b,p])))
         const collidedBlockWithBlock = () => allActiveAndPlacedBlocks().filter(t => t[0].x+1 == t[1].x && t[0].y == t[1].y).length > 0
-        const collidedRight = () => getRightmostBlock(s.tetromino).x == 9
+        const collidedRightBoundary = () => getRightmostBlock(s.tetromino).x == 9
         return {
             ...s,
-            tetromino: collidedBlockWithBlock() ? s.tetromino : s.tetromino.map(b=> {
+            tetromino: collidedBlockWithBlock() || collidedRightBoundary() ? s.tetromino : s.tetromino.map(b=> {
                 return {
                     ...b,
-                    x: collidedRight() ? b.x : (b.x+this.changes)
+                    x: b.x+this.changes
                 }
             })
         }
